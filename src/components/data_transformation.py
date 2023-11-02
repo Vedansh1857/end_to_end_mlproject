f10a2954ac3d710ff0800ee5ed55aa2a2acfa39c
Vedansh Gupta <vedanshgupta606@gmail.com>
2023-11-02 23:24:09 +0530

    .

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -101,9 +101,7 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
