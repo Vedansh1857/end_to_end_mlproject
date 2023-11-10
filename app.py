d4100098dbcf3764a62bd86e1f4b338978706c06
Vedansh Gupta <vedanshgupta606@gmail.com>
2023-11-10 21:40:28 +0530

    xyz

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ app = application
 def index():
     return render_template('index.html')
 
-
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method == 'GET':
